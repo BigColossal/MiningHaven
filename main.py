6ba555777f019b68b2078a20c1594c893948fe73
BigColossal <jeremyperez1409@gmail.com>
2025-07-30 20:47:54 -0400

    Imported sprite sheets, and loaded them into cache.

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame as pg
+SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 1000
+
+pg.init()
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    print("Hello from topmininggame!")
+
+
+if __name__ == "__main__":
+    main()
